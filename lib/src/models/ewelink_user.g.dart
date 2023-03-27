2554612c64a8985d8cd378a271f6f9896d6416b4
Ian Koerich Maciel <ianmaciel@gmail.com>
2023-03-27 13:05:24 -0300

    fix: user object model
    Some users has different data on user's object. This commit fix by
    marking as nullable properties that we are now sure if will be
    available or not.

--- lib/src/models/ewelink_user.g.dart
+++ lib/src/models/ewelink_user.g.dart
@@ -13,13 +13,15 @@ EwelinkUser _$EwelinkUserFromJson(Map<String, dynamic> json) => EwelinkUser(
           .map((e) => EwelinkAppInfos.fromJson(e as Map<String, dynamic>))
           .toList(),
       json['bindInfos'] as Map<String, dynamic>,
-      EwelinkAccountInfo.fromJson(json['accountInfo'] as Map<String, dynamic>),
+      json['accountInfo'] == null
+          ? null
+          : EwelinkAccountInfo.fromJson(
+              json['accountInfo'] as Map<String, dynamic>),
       EwelinkClientInfo.fromJson(json['clientInfo'] as Map<String, dynamic>),
       json['countryCode'] as String,
       json['createdAt'] as String,
       json['currentFamilyId'] as String,
       json['email'] as String,
-      json['extra'] as Map<String, dynamic>,
       json['extraPush'] as Map<String, dynamic>,
       json['ip'] as String,
       json['isAccepEmailAd'] as bool,
@@ -29,8 +31,10 @@ EwelinkUser _$EwelinkUserFromJson(Map<String, dynamic> json) => EwelinkUser(
       json['offlineTime'] as String,
       json['online'] as bool,
       json['password'] as String,
-      json['platformLang'] as Map<String, dynamic>,
+      json['platformLang'] as Map<String, dynamic>?,
       json['userStatus'] as String,
+      json['timezone'] as Map<String, dynamic>?,
+      json['extra'] as Map<String, dynamic>,
     );
 
 Map<String, dynamic> _$EwelinkUserToJson(EwelinkUser instance) =>
@@ -39,13 +43,12 @@ Map<String, dynamic> _$EwelinkUserToJson(EwelinkUser instance) =>
       'appId': instance.appId,
       'appInfos': instance.appInfos.map((e) => e.toJson()).toList(),
       'bindInfos': instance.bindInfos,
-      'accountInfo': instance.accountInfo.toJson(),
+      'accountInfo': instance.accountInfo?.toJson(),
       'clientInfo': instance.clientInfo.toJson(),
       'countryCode': instance.countryCode,
       'createdAt': instance.createdAt,
       'currentFamilyId': instance.currentFamilyId,
       'email': instance.email,
-      'extra': instance.extra,
       'extraPush': instance.extraPush,
       'ip': instance.ip,
       'isAccepEmailAd': instance.isAccepEmailAd,
@@ -57,4 +60,6 @@ Map<String, dynamic> _$EwelinkUserToJson(EwelinkUser instance) =>
       'password': instance.password,
       'platformLang': instance.platformLang,
       'userStatus': instance.userStatus,
+      'timezone': instance.timezone,
+      'extra': instance.extra,
     };
